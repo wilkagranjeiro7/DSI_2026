0a4f5b868a8e7292423fd8a128e292402db9adb1
Wilka Granjeiro <wilka.granjeiro@ufrpe.br>
2026-05-24 11:27:08 -0300

    feat: mapa completo com autocomplete, novos filtros e navegacao na home

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout() {
         <Stack.Screen name="login" />
         <Stack.Screen name="signup" />
         <Stack.Screen name="home" />
+        <Stack.Screen name="map" />
         <Stack.Screen name="meus-treinos" />
         <Stack.Screen name="meus-exercicios" />
         <Stack.Screen name="criar-treino" />
