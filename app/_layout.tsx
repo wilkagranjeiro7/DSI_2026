c87f4061f45116e0f24957884c925ca8bade91c1
Kassiane Silva <kassiane.gsilva@ufrpe.br>
2026-06-14 13:10:03 -0300

    refatora: altera rotas para paradigma de orientação a objetos

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,26 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
 
-export default function RootLayout() {
-  return (
-    <>
-      <StatusBar style="dark" />
+export default class RootLayout extends Component {
+  render() {
+    return (
+      <>
+        <StatusBar style="dark" />
 
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="login" />
-        <Stack.Screen name="signup" />
-        <Stack.Screen name="home" />
-        <Stack.Screen name="map" />
-        <Stack.Screen name="meus-treinos" />
-        <Stack.Screen name="meus-exercicios" />
-        <Stack.Screen name="criar-treino" />
-        <Stack.Screen name="detalhes" />
-        <Stack.Screen name="biblioteca" />
-      </Stack>
-    </>
-  );
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="login" />
+          <Stack.Screen name="signup" />
+          <Stack.Screen name="home" />
+          <Stack.Screen name="map" />
+          <Stack.Screen name="meus-treinos" />
+          <Stack.Screen name="meus-exercicios" />
+          <Stack.Screen name="criar-treino" />
+          <Stack.Screen name="detalhes" />
+          <Stack.Screen name="biblioteca" />
+        </Stack>
+      </>
+    );
+  }
 }
